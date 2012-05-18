e7d45c3e4e6e145c9fc83da6696afb9197d777a6
GordonDiggs <gordon@gordondiggs.com>
2012-05-18 15:46:19 -0400

    prepare for heroku deploy

--- hey_buddy.rb
+++ hey_buddy.rb
@@ -8,6 +8,7 @@ require "sinatra/config_file"
 
 configure do
   config_file 'settings.yml'
+  set :instagram_client_id, ENV['INSTAGRAM_CLIENT_ID'] if ENV['INSTAGRAM_CLIENT_ID'] # only because heroku doesn't let you do settings.yml files
 end
 
 get '/' do
